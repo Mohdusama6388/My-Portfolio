099621d1d04eb3393e76927010ef56bd67ac4049
Mohd Usama <mohdusama2362004@gmail.com>
2025-09-18 21:47:22 +0530

    Modified home page code

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import Pdf from '../assets/Usama.Pdf';
+
 import Profile from '../assets/image.png'; 
 import Style from './Home.module.css';
 
@@ -14,9 +14,8 @@ function Home() {
           </h1>
           <h2 className={Style.h2}>A Passionate MERN Stack Developer </h2>
           <h3 className={Style.h3}>I am a passionate web developer skilled in building responsive and user-friendly applications using the MERN stack (MongoDB, Express.js, React.js, and Node.js). I focus on writing clean, efficient, and maintainable code while ensuring smooth user experiences. As a fresher, I am eager to learn, adapt, and grow by exploring new technologies and contributing to innovative projects. My goal is to apply my skills effectively and create impactful solutions that add value. </h3>
-
-          <a href={Pdf} download={'Usama resume'}>
-            <button className={Style.btn}> My Resume</button>
+          <a  className={Style.btn}   href="/Usama.pdf" target="_blank" rel="noopener noreferrer">
+       My Resume 
           </a>
         </div>
       </div>
